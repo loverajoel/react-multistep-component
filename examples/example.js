4e8565bbc5c7c3ac3449a927ce626d68b2284adf
Joel <joelalovera@gmail.com>
2016-03-19 18:05:37 -0300

    add config for babel and first steps

--- examples/example.js
+++ examples/example.js
@@ -1,1 +1,21 @@
-console.log('hello')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Steps, Step } from '../lib';
+
+class Example extends React.Component {
+  render() {
+    return (
+      <Steps currentStep={1}>
+        <Step>
+          Step1
+        </Step>
+        <Step>
+          Step2
+        </Step>
+      </Steps>
+      );
+  }
+}
+
+ReactDOM.render(<Example/> , document.getElementById('example'));
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, './examples/dist')
+var APP_DIR = path.resolve(__dirname, './')
+
+var config = {
+  entry: [
+    APP_DIR + '/examples/example.js',
+    APP_DIR + '/lib/styles.scss',
+  ],
+  output: {
+    path: BUILD_DIR,
+    filename: '/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: APP_DIR,
+        loader : 'babel'
+      },
+      {
+        test: /\.scss/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract(
+          'style',
+          'css!sass'
+        )
+      },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin( "bundle.css" )
+  ],
+  watch: true,
+  devtool: 'source-map'
+};
+
+module.exports = config;
