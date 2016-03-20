5fcfdfa22c173f42bc94fb3ee518060332b66e26
Joel <joelalovera@gmail.com>
2016-03-20 04:03:37 -0300

    add navigator element

--- examples/example.js
+++ examples/example.js
@@ -5,13 +5,17 @@ import { Steps, Step } from '../lib';
 
 class Example extends React.Component {
   render() {
+    let nextButton = (<span>nExT</span>);
     return (
-      <Steps currentStep={1}>
+      <Steps currentStep={1} prevButton='prev' nextButton={nextButton}>
         <Step>
           Step1
         </Step>
         <Step>
           Step2
+        </Step>
+        <Step>
+          Step3
         </Step>
       </Steps>
     );
