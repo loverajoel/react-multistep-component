c25505db56b0e717dfb7669c81281e9e219ac432
Joel <joelalovera@gmail.com>
2016-03-20 20:07:01 -0300

    add coverage

--- karma.conf.js
+++ karma.conf.js
@@ -12,17 +12,27 @@ module.exports = function (config) {
     preprocessors: {
       'tests.webpack.js': [ 'webpack', 'sourcemap' ] //preprocess with webpack and our sourcemap loader
     },
-    reporters: [ 'dots' ], //report results in this format
+    reporters: [ 'dots', 'coverage' ], //report results in this format
     webpack: { //kind of a copy of your webpack config
       devtool: 'inline-source-map', //just do inline source maps instead of the default
       module: {
         loaders: [
           { test: /\.js$/, loader: 'babel-loader' }
-        ]
+        ],
+        postLoaders: [ {
+          test: /\.js$/,
+          exclude: /(__test__|node_modules|examples)\//,
+          loader: 'istanbul-instrumenter'
+        } ]
       }
     },
     webpackServer: {
       noInfo: true //please don't spam the console when running in karma!
+    },
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage',
+      subdir: '.'
     }
   });
 };
